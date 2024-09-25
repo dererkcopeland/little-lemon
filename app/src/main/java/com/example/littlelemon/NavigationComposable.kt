405ce921eec435a3a29df490a350314caaaf4ecf
dererkcopeland <125219246+dererkcopeland@users.noreply.github.com>
2024-09-25 12:30:58 -0400

    Updated Onboarding and added Home and Profile files.

---
+++ app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -0,0 +1,37 @@
+package com.example.littlelemon
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun Navigation(navController: NavHostController){
+
+    val sharedPreferences = LocalContext.current.getSharedPreferences("LittleLemon", Context.MODE_PRIVATE)
+    val email = sharedPreferences.getString("email", "")
+    val firstName = sharedPreferences.getString("firstName", "")
+    val lastName = sharedPreferences.getString("lastName", "")
+    val isRegistered = !email.isNullOrBlank() && !firstName.isNullOrBlank() && !lastName.isNullOrBlank()
+
+    var startDestination: String = Home.route
+
+    if(!isRegistered){
+        startDestination = Onboarding.route
+    }
+
+    NavHost(navController = navController, startDestination = startDestination){
+        composable(Home.route){
+            Home()
+        }
+        composable(Onboarding.route){
+            Onboarding(navController)
+        }
+        composable(Profile.route){
+            Profile()
+        }
+    }
+}
--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -1,5 +1,7 @@
 package com.example.littlelemon
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,6 +22,7 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,12 +30,18 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun Onboarding () {
+fun Onboarding (navController: NavHostController) {
     var firstName by remember { mutableStateOf("") }
     var lastName by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
+
+    var error by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
 
     Column{
         Image(
@@ -91,15 +100,43 @@ fun Onboarding () {
                 containerColor = colorResource(id = R.color.yellow),
                 contentColor = colorResource(id = R.color.green)
             ),
-            onClick = { /*TODO*/ }) {
+            onClick = {
+                error = validateRegistration(firstName, lastName, email, context)
+                if (error.isBlank()) {
+                    Toast.makeText(context, "Registration successful!", Toast.LENGTH_SHORT).show()
+                    navController.navigate(Home.route)
+                }
+            }) {
             Text(text = "Register")
         }
+        Text(
+            modifier = Modifier.padding(10.dp),
+            text = error,
+            color = Color.Red
+        )
     }
 
+}
+
+fun validateRegistration(firstName: String, lastName: String, email: String, context: Context): String {
+    if (firstName.isBlank() || lastName.isBlank() || email.isBlank()) {
+        return "Registration unsuccessful. Please enter all data."
+    } else if (!email.contains("@") || !email.contains(".")) {
+        return "Invalid email format."
+    } else {
+        // Save Info to saved Preferences
+        val sharedPreferences = context.getSharedPreferences("LittleLemon", Context.MODE_PRIVATE)
+        sharedPreferences.edit()
+            .putString("firstName", firstName)
+            .putString("lastName", lastName)
+            .putString("email", email)
+            .apply()
+        return ""
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun OnboardingPreview() {
-    Onboarding()
+    Onboarding(rememberNavController())
 }
